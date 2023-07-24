87604c24f092b80d7bd3d3d3006d325a515da8aa
bob24170 <58536544+bob24170@users.noreply.github.com>
2023-07-24 18:39:15 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 import firebase from "firebase/app"
 import "firebase/firestore"
 
-var firebaseConfig = {
+//var firebaseConfig = {
   apiKey: "AIzaSyApSxanyBb6D2IVtFCL29dHsRaq4xV2CG4",
   authDomain: "blog-comments-995eb.firebaseapp.com",
   databaseURL: "https://blog-comments-995eb.firebaseio.com",
@@ -10,8 +10,27 @@ var firebaseConfig = {
   messagingSenderId: "335918297592",
   appId: "1:335918297592:web:30f56d162560dc1df6260d"
 }
+//
 
-firebase.initializeApp(firebaseConfig)
+//firebase.initializeApp(firebaseConfig)
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDnNLiZcHBUxqVXT7H7xBMiFqAoo-Hr9nI",
+  authDomain: "comment-66100.firebaseapp.com",
+  projectId: "comment-66100",
+  storageBucket: "comment-66100.appspot.com",
+  messagingSenderId: "192154982574",
+  appId: "1:192154982574:web:06640213fe8ff353ea353f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 export const firestore = firebase.firestore()
 

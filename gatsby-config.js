c1b8fbd9f91b06f04f10307dcc72e766824ba784
bob24170 <58536544+bob24170@users.noreply.github.com>
2023-08-17 21:21:26 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: `#319795`,
         theme_color: `#319795`,
         display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality

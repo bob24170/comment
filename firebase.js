ad315f75103f21fc9f8489900f7b021446742d05
bob24170 <58536544+bob24170@users.noreply.github.com>
2023-08-18 20:13:16 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -24,7 +24,7 @@
 //Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 //TODO: Add SDKs for Firebase products that you want to use
-import { } from 'firebase/firestore';
+import { firestore } from 'firebase/firestore';
 
 //Your web app's Firebase configuration
 const firebaseConfig = {
